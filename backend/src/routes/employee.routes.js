71a8c60460a2751226344ec6f8410fc05b7da308
vaishalis04 <vaishalisingh0482@gmail.com>
2026-09-14 02:26:58 +0530

    employees by company id

--- backend/src/routes/employee.routes.js
+++ backend/src/routes/employee.routes.js
@@ -1,37 +1,71 @@
 const express = require("express");
+
 const router = express.Router();
+
 const Controller = require("../controllers/employee.controller");
-const { protect } = require("../middleware/auth.middleware");
+
+const { protect ,superAdminOnly, adminOnly, adminOrHR } = require("../middleware/auth.middleware");
+
 const { authorize } = require("../middleware/role.middleware");
+
 const upload = require("../middleware/upload.middleware");
 
 router.use(protect);
 
-router
-  .route("/")
-  .get(Controller.getEmployees)
-  .post(
-    authorize("company_admin", "hr","super_admin"),
-    (req, res, next) => {
-      req.uploadFolder = "profiles";
-      next();
-    },
-    upload.single("profileImage"),
-    Controller.createEmployee,
-  );
+router.get(
+  "/my-profile",
+  Controller.getMyProfile
+);
 
-router
-  .route("/:id")
-  .get(Controller.getEmployee)
-  .put(
-    authorize("company_admin", "hr","super_admin"),
-    (req, res, next) => {
-      req.uploadFolder = "profiles";
-      next();
-    },
-    upload.single("profileImage"),
-    Controller.updateEmployee,
-  )
-  .delete(authorize("company_admin","super_admin"), Controller.deleteEmployee);
+router.get(
+  "/my-leave-balance",
+  Controller.getLeaveBalance
+);
+
+router.get(
+  "/company/:companyId",
+  // superAdminOnly, adminOnly, adminOrHR,
+  // authorize("company_admin", "hr", "super_admin"),
+  Controller.getEmployeesByCompanyId
+);
+
+router.get(
+  "/",
+  Controller.getEmployees
+);
+
+router.post(
+  "/",
+  authorize("company_admin", "hr", "super_admin"),
+  (req, res, next) => {
+    req.uploadFolder = "profiles";
+    next();
+  },
+  upload.single("profileImage"),
+  Controller.createEmployee
+);
+
+router.get(
+  "/:id",
+  Controller.getEmployee
+);
+
+router.put(
+  "/:id",
+  authorize("company_admin", "hr", "super_admin"),
+  (req, res, next) => {
+    req.uploadFolder = "profiles";
+    next();
+  },
+  upload.single("profileImage"),
+  Controller.updateEmployee
+);
+
+router.delete(
+  "/:id",
+  authorize("company_admin", "super_admin"),
+  Controller.deleteEmployee
+);
 
 module.exports = router;
+
